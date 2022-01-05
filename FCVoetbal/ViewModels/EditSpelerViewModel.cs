bc835d2ee9c3e93026335ec41a2115ccc49dfef9
Justinderuijer <justin.xepos.deruijter@gmail.com>
2022-01-05 16:59:16 +0100

    Speler CRUD

---
+++ FCVoetbal/ViewModels/EditSpelerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FCVoetbal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FCVoetbal.ViewModels
+{
+    public class EditSpelerViewModel : CreateSpelerViewModel
+    {
+        public EditSpelerViewModel(ICollection<Team> teamOptions, int id, string voornaam, string achternaam, int? rugnummer, int doelpunten, int teamID = 0) : base(teamOptions ,voornaam, achternaam, rugnummer, doelpunten, teamID)
+        {
+            ID = id;
+        }
+
+        public int ID { get; set; }
+    }
+}
---
+++ FCVoetbal/ViewModels/SpelerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FCVoetbal.ViewModels
+{
+    public class SpelerViewModel
+    {
+        public SpelerViewModel()
+        {
+
+        }
+        public SpelerViewModel(string voornaam, string achternaam, int? rugnummer, int doelpunten, int teamID = 0)
+        {
+            Voornaam = voornaam;
+            Achternaam = achternaam;
+            Rugnummer = rugnummer;
+            Doelpunten = doelpunten;
+            TeamID = teamID;
+        }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public int? Rugnummer { get; set; }
+        public int Doelpunten { get; set; }
+        public int TeamID { get; set; }
+    }
+}
