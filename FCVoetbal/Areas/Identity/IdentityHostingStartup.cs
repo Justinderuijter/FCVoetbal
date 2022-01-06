7613a47942c53b6260b040d4bc66c46aaeb1eef7
Justinderuijer <justin.xepos.deruijter@gmail.com>
2022-01-06 16:05:18 +0100

    Scaffolding

---
+++ FCVoetbal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FCVoetbal.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FCVoetbal.Areas.Identity.IdentityHostingStartup))]
+namespace FCVoetbal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FCVoetbalContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("LocalDBConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequiredUniqueChars = 1;
+
+
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                    options.Lockout.MaxFailedAccessAttempts = 5;
+                    options.Lockout.AllowedForNewUsers = true;
+
+                    options.User.RequireUniqueEmail = true;
+
+                })
+                    .AddEntityFrameworkStores<VoetbalContext>();
+            });
+        }
+    }
+}
